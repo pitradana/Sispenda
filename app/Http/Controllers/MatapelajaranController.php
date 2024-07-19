8b88c70e651ea9367fee487b7ca46c8db0e33e6c
Antonius Pitra Dana Arista <pitradana@users.noreply.github.com>
2024-07-19 11:33:23 +0700

    aplikasi begin

---
+++ app/Http/Controllers/MatapelajaranController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Matapelajaran;
+use Illuminate\Http\Request;
+
+class MatapelajaranController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Matapelajaran $matapelajaran)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Matapelajaran $matapelajaran)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Matapelajaran $matapelajaran)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Matapelajaran $matapelajaran)
+    {
+        //
+    }
+}
